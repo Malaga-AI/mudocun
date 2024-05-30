ab93b84bc0e3359e44c788338c0a735f193bc077
Diego Regules <regutonlabs@gmail.com>
2024-05-30 14:19:58 +0200

    few docs added

--- docs.py
+++ docs.py
@@ -11,8 +11,31 @@ local_documents = [
 
 online_documents = [
     {"title": "Mixtral of Experts", "uri": "https://arxiv.org/pdf/2401.04088"},
+    {"title": "Principles of bursty mRNA expression and irreversibility in single cells and extrinsically varying populations", "uri": "https://arxiv.org/pdf/2405.12897"},
     {
-        "title": "Principles of bursty mRNA expression and irreversibility in single cells and extrinsically varying populations",
-        "uri": "https://arxiv.org/pdf/2405.12897",
+        "title": "Cancer statistics, 2024",
+        "uri": "https://onlinelibrary.wiley.com/doi/pdfdirect/10.3322/caac.21820",
     },
+    {
+        "title": "Nodular/Keloidal Scleroderma with No Systemic Involvement—A Case Report and a Review of the Literature",
+        "uri": "https://www.mdpi.com/2077-0383/13/9/2662/pdf?version=1714557569",
+    },
+    {
+        "title": "Operations Research, Mathematics, Computer Science and Statistics: The Relationships",
+        "uri": "https://talenta.usu.ac.id/JoCAI/article/download/653/2722",
+    },
+    {
+        "title": "Biotechnology, Physics, Computer Science",
+        "uri": "https://www.nature.com/articles/35108009.pdf",
+    },
+    {
+        "title": "The 6th International Conference on Computer Science and Computational Mathematics (ICCSCM 2017)",
+        "uri": "https://iopscience.iop.org/article/10.1088/1742-6596/892/1/011001/pdf",
+    },
+    {
+        "title": "Show issue Year 03/2024 Volume 16 Issue 1 Objectives of Public Finance Law and Axiological Analysis of Law – the Guiding Principles and a Proposal for a Research Approach",
+        "uri": "https://journals.kozminski.edu.pl/system/files/Zawadzka-Pak_EN.pdf"
+    }
 ]
+
+# Voy a ir anadiendo mas documentos
